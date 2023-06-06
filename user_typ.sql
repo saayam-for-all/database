499b68d3b4b4591fb14f3bfbaed97030d37e218e
Mohanakrishnan <67235801+mohankrishnan83@users.noreply.github.com>
2023-06-06 22:05:16 +0530

    User types
    This table is used to categorize the  different types of users.

---
+++ user_typ.sql
@@ -0,0 +1,25 @@
+/********************************************************************
+**
+**  Application : Saayam 
+**  
+**  Table Name  : user_typ 
+**
+**  Purpose  : A Table to store types of users.  
+**
+**  created dt : 04/04/2023
+**
+**  Last Modified : 
+**
+**  Change Comments : 
+**
+**
+**
+**********************************************************************/
+
+drop table if exists user_typ;
+
+create table user_typ (
+usr_typ_id integer generated always as identity primary key,
+usr_typ varchar(30) not null,
+usr_typ_desc varchar(50) not null
+);
