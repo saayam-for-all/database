75e4319a3238241544fef040f70e05d8879c8f16
Mohanakrishnan <67235801+mohankrishnan83@users.noreply.github.com>
2023-06-11 16:21:00 +0530

    Update user_typ.sql
    Volunteer or any type of user entity that holds information related user types and user attributes .  
    
    usr_typ entity version :  New v1

--- user_typ.sql
+++ user_typ.sql
@@ -1,25 +1,31 @@
-/********************************************************************
-**
-**  Application : Saayam 
-**  
-**  Table Name  : user_typ 
-**
-**  Purpose  : A Table to store types of users.  
-**
-**  created dt : 04/04/2023
-**
-**  Last Modified : 
-**
-**  Change Comments : 
-**
-**
-**
-**********************************************************************/
+/*************************************************************
+***
+***  Name       :  usr_typ.sql 
+***  purpose    :  To identigy snf categorize the group of users. 
+***  Depemdency :  Down stream dependency with Usr Table. 
+***  
+***
+*** Data Model: https://drawsql.app/teams/sayam-team/diagrams/saayam-arch
+***
+*** Created on   : 06/10/2023 
+*** Created By   : 
+*** Last Updatee :
+*** Last update by : 
+*** change comments: 
+***
+***
+******************************************************************/
 
-drop table if exists user_typ;
+drop table if exists usr_typ ;
 
-create table user_typ (
-usr_typ_id integer generated always as identity primary key,
-usr_typ varchar(30) not null,
-usr_typ_desc varchar(50) not null
-);
+create table if not exists usr_typ 
+( 
+    usr_typ_id   integer generated always as identity
+   ,usr_typ      varchar(15) not null 
+   ,usr_typ_dsc varchar(30) not null 
+   ,cre_dt       date default current_date 
+   ,cre_by       varchar(30) default 'SYSTEM' 
+   ,lst_upd      date 
+   ,lst_upd_by   varchar(30) 
+   ,CONSTRAINT  uk_ut_utypid  unique(usr_typ_id) 
+ );
