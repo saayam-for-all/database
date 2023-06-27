b3b011f922add1067091ac761d9714e95177db5e
Mohanakrishnan <67235801+mohankrishnan83@users.noreply.github.com>
2023-06-27 20:54:01 +0530

    Add files via upload

---
+++ dml/dml_region.sql
@@ -0,0 +1,24 @@
+/*************************************************************
+***
+***  Name       :  dml_region.sql 
+***  purpose    :  list of region entries.
+***                
+***  Dependency :  region  
+***  
+***
+*** Data Model: https://drawsql.app/teams/sayam-team/diagrams/saayam-arch
+***
+*** Created on   : 06/29/2023 
+*** Created By   : 
+*** Last Updatee :
+*** Last update by : 
+*** change comments: 
+***
+***
+******************************************************************/
+
+insert into region ( regn_nm , cre_dt , cre_by) values ( 'Americas' , current_date , 'SYSTEM' );
+insert into region ( regn_nm , cre_dt , cre_by) values ( 'Asia Pacific' , current_date , 'SYSTEM' );
+insert into region ( regn_nm , cre_dt , cre_by) values ( 'Europe' , current_date , 'SYSTEM' );
+insert into region ( regn_nm , cre_dt , cre_by) values ( 'Middle East' , current_date , 'SYSTEM' );
+insert into region ( regn_nm , cre_dt , cre_by) values ( 'Africas' , current_date , 'SYSTEM' );
