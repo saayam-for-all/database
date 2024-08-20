dd03443ca33d40d6f8e3419957f9659ec9a09f3f
Sayali Oak <sayali.oak.52@gmail.com>
2024-08-20 17:56:11 -0400

    user_skills error resolved

--- ddl/Tables/Using PostgreSQL and pgAdmin4 Tool/PostgreSQL-Saayam-Proposed-Schema.sql
+++ ddl/Tables/Using PostgreSQL and pgAdmin4 Tool/PostgreSQL-Saayam-Proposed-Schema.sql
@@ -277,7 +277,7 @@ CREATE TABLE IF NOT EXISTS skill_list (
     request_category_id INT NOT NULL,
     skill_desc VARCHAR(100) NOT NULL,
     last_update_date TIMESTAMP,
-    UNIQUE (skill_lst_id),
+    UNIQUE (skill_list_id),
     FOREIGN KEY (request_category_id) REFERENCES request_category (request_category_id)
 );
 
