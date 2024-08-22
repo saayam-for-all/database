382873c4560b5556455834070424ef29359c7e2a
Sayali Oak <sayali.oak.52@gmail.com>
2024-08-22 12:25:33 -0400

    modified user skills table

--- ddl/Tables/Using PostgreSQL and pgAdmin4 Tool/PostgreSQL-Saayam-Proposed-Schema.sql
+++ ddl/Tables/Using PostgreSQL and pgAdmin4 Tool/PostgreSQL-Saayam-Proposed-Schema.sql
@@ -291,11 +291,14 @@ CREATE TABLE IF NOT EXISTS sla (
     UNIQUE (sla_id)
 );
 
+CREATE TYPE skill_levels AS ENUM('BEGINNER', 'INTERMEDIATE', 'ADVANCED', 'EXPERT');
+
 -- Table: user_skills
 CREATE TABLE IF NOT EXISTS user_skills (
 	user_skills_id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     skill_id INT NOT NULL,
+    skill_level skill_levels,
     last_used_date TIMESTAMP,
     created_date TIMESTAMP,
     last_update_date TIMESTAMP,
