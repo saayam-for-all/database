a1d1ee08d0b08019e1ac287f59340d20c56ea2d4
PallaviChitrada <pallavichitrada10@gmail.com>
2025-07-27 12:34:14 -0400

    Update ddl_req_add_info_metadata.sql
    Added Foreign key as per FaraFatima's request.

--- ddl/Tables/ddl_req_add_info_metadata.sql
+++ ddl/Tables/ddl_req_add_info_metadata.sql
@@ -5,10 +5,10 @@ create table if not exists req_add_info_metadata (
     field_name_key VARCHAR(100),
     field_type VARCHAR(20),                        -- 'string', 'int', 'float'
     status VARCHAR(10) DEFAULT 'active' CHECK (status IN ('active', 'inactive')),           -- 'active' or 'inactive'
-    PRIMARY KEY (cat_id, field_name_key)
+    PRIMARY KEY (cat_id, field_name_key),
+    FOREIGN KEY (cat_id) REFERENCES help_category(cat_id)
 );
 
--- Do you need to add FOREIGN KEY (category_id) REFERENCES Help_Category(category_id)?
 -- Suggested Change: Consider using ENUM for status to avoid invalid text values
 
 /* insertion is done by req_add_info_metadata.csv file */
