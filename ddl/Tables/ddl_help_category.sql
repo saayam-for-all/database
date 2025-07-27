ff6c65d7a55e5764cdab471bdbfa8a16bfd93484
PallaviChitrada <pallavichitrada10@gmail.com>
2025-07-27 15:54:50 -0400

    Update ddl_help_category.sql

--- ddl/Tables/ddl_help_category.sql
+++ ddl/Tables/ddl_help_category.sql
@@ -1,6 +1,6 @@
 drop table if exists help_category
 
-create table if not exists TABLE help_category (
+create table if not exists help_category (
     cat_id VARCHAR(50) PRIMARY KEY,           -- e.g., '1', '1.1', '1.1.1'
     cat_name VARCHAR(100) NOT NULL,                    -- string_key, e.g., 'DONATE_CLOTHES'
     cat_desc TEXT NOT NULL,                     -- purpose or goal of the category
