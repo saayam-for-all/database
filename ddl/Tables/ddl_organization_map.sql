a5a0805d06a9bd9d12aea980905992acb758eb44
PallaviChitrada <pallavichitrada10@gmail.com>
2025-07-27 12:53:19 -0400

    Changes in insertion of organization table & creation of mapping table

---
+++ ddl/Tables/ddl_organization_map.sql
@@ -0,0 +1,30 @@
+drop table if exists organization_map
+
+create table if not exists organization_map (
+    parent_org_id VARCHAR(50),
+    child_org_id VARCHAR(50) PRIMARY KEY, --one parent multiple child
+    FOREIGN KEY (parent_org_id) REFERENCES organization(org_id),
+    FOREIGN KEY (child_org_id) REFERENCES organization(org_id)
+);
+
+/* insertion is done by organization_map.csv file 
+sample csv file:
+parent_org_id      child_org_id
+                   ORG-1
+ORG-1.1            ORG-1
+ORG-1.2            ORG-1
+ORG-1.3            ORG-1
+                   ORG-2
+ORG-2.1            ORG-2
+ORG-2.2            ORG-2
+ORG-2.2.1          ORG-2.2
+
+the .csv file has to be determined like this and manually populated into the db
+where, the parent would be the head branch, and the child would be the other branches
+for example, ORG-2 is of Red Cross, Washington, D.C
+ORG-2.1 is Red Cross, Newark
+ORG-2.2 is Red Cross, OH one of the chapter
+ORG-2.2.1 is Red Cross, Columbus, OH would be local chapter
+
+the collection should be done by data engineering team
+*/
