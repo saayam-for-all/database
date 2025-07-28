d9bb0bbf0e207dda99a0239873a1f5dab45e3201
PallaviChitrada <pallavichitrada10@gmail.com>
2025-07-27 22:48:06 -0400

    Update ddl_organization.sql

--- ddl/Tables/ddl_organization.sql
+++ ddl/Tables/ddl_organization.sql
@@ -3,7 +3,7 @@
 create table if not exists organization (
   org_id VARCHAR(255) PRIMARY KEY,
   org_name VARCHAR(255) NOT NULL,
-  emp_id_num VARCHAR(15) UNIQUE,
+  emp_id_num VARCHAR(15),
   mission_statement TEXT,
   website_url VARCHAR(255),
   phone VARCHAR(20),
@@ -14,6 +14,7 @@ create table if not exists organization (
   data_source VARCHAR(20) CHECK (data_source IN ('IRS', 'Self-Registered')),
   people JSON  -- Future volunteer contact storage
   cat_id VARCHAR(50),
+  UNIQUE(emo_id_num),
   FOREIGN KEY (cat_id) REFERENCES help_category(cat_id) --Linking up help_category with organization
 );
  
