955c0bab01b65dbd8ad675f3f4fa14804a362fef
PallaviChitrada <pallavichitrada10@gmail.com>
2025-08-12 13:36:31 -0500

    Update ddl_help_categories.sql

--- ddl/Tables/ddl_help_categories.sql
+++ ddl/Tables/ddl_help_categories.sql
@@ -3,7 +3,7 @@
 create table if not exists virginia_dev_saayam_rdbms.help_categories (
     cat_id VARCHAR(50) PRIMARY KEY,           -- e.g., '1', '1.1', '1.1.1'
     cat_name VARCHAR(100) NOT NULL,                    -- string_key, e.g., 'DONATE_CLOTHES'
-    cat_desc TEXT NOT NULL                     -- purpose or goal of the category
+    cat_desc VARCHAR(150) NOT NULL                     -- purpose or goal of the category
 );
 
 /* insertion is done by help_categories.csv file */
