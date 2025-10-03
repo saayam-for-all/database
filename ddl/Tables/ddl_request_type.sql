fe06a0909c38b035a6ee8d87ff03cd25f4ac4662
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 21:49:01 -0500

    Update ddl_request_type.sql

--- ddl/Tables/ddl_request_type.sql
+++ ddl/Tables/ddl_request_type.sql
@@ -1,7 +1,7 @@
 -- Table: request_type
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_type (
-    request_type_id SERIAL PRIMARY KEY,
-    request_type VARCHAR(255),
-    request_type_desc VARCHAR(255),
+    req_type_id SERIAL PRIMARY KEY,
+    req_type VARCHAR(25),
+    req_type_desc VARCHAR(125),
     last_updated_date TIMESTAMP
 );
