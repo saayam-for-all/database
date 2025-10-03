4d96b93141d75ae11e2fd46bdb70060642fc5fd1
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 21:48:01 -0500

    Update ddl_request_priority.sql

--- ddl/Tables/ddl_request_priority.sql
+++ ddl/Tables/ddl_request_priority.sql
@@ -1,8 +1,8 @@
 -- Table: request_priority
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_priority (
-    priority_id SERIAL PRIMARY KEY,
-    priority_value VARCHAR(25) NOT NULL,
-    priority_desc VARCHAR(125),
+    req_priority_id SERIAL PRIMARY KEY,
+    req_priority VARCHAR(25) NOT NULL,
+    req_priority_desc VARCHAR(125),
     last_updated_date TIMESTAMP,
     UNIQUE (priority_id)
 );
