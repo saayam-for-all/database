817e0d36028bb3e727e85c43910a841a9c8d4e17
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 21:47:03 -0500

    Update ddl_request_status.sql

--- ddl/Tables/ddl_request_status.sql
+++ ddl/Tables/ddl_request_status.sql
@@ -1,7 +1,7 @@
 -- Table: request_status
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_status (
-    request_status_id SERIAL PRIMARY KEY,
-    request_status VARCHAR(255) NOT NULL,
-    request_status_desc VARCHAR(255),
+    req_status_id SERIAL PRIMARY KEY,
+    req_status VARCHAR(25) NOT NULL,
+    req_status_desc VARCHAR(125),
     last_updated_date TIMESTAMP
 );
