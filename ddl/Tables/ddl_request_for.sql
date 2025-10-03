9c4c03eab02f6845fe98601062d14bbf6a81b302
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 21:57:53 -0500

    Update ddl_request_for.sql

--- ddl/Tables/ddl_request_for.sql
+++ ddl/Tables/ddl_request_for.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_for (
     req_for_desc VARCHAR(125), --15words7charlength
     last_updated_date TIMESTAMP
 );
+
+
+-- CSV
+-- 0   SELF    Personal benefits of the individual
+-- 1   OTHER   User is filing a request for others. This other person does NOT have an account within our system. 
