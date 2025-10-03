a33d57d6b9c3c08a39c99c75d078deda337ac42d
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 22:00:24 -0500

    Update ddl_request_status.sql

--- ddl/Tables/ddl_request_status.sql
+++ ddl/Tables/ddl_request_status.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_status (
     req_status_desc VARCHAR(125),
     last_updated_date TIMESTAMP
 );
+
+-- CSV
+-- 0  CREATED
+-- 1  MATCHING VOLUNTEER
+-- 2  MANAGED
+-- 3  CLOSED
+-- 4  CANCELLED
+-- 5  DELETED
