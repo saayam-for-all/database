59e3ba5173eff8cddb500aa9270e1c1ab4b5ca4b
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-02 21:40:55 -0500

    Update ddl_request_for.sql

--- ddl/Tables/ddl_request_for.sql
+++ ddl/Tables/ddl_request_for.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_for (
     request_for_id SERIAL PRIMARY KEY,
-    request_for VARCHAR(255) NOT NULL,
-    request_for_desc VARCHAR(255),
+    request_for VARCHAR(25) NOT NULL,  
+    request_for_desc VARCHAR(125), --15words7charlength
     last_updated_date TIMESTAMP
 );
