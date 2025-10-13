624cff47b799ba0dd5bc1e124fc99e751c364e91
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 21:16:24 -0400

    Update ddl_volunteers_assigned.sql

--- ddl/Tables/ddl_volunteers_assigned.sql
+++ ddl/Tables/ddl_volunteers_assigned.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.volunteers_assigned (
     volunteer_id VARCHAR(255) NOT NULL,
     volunteer_type VARCHAR(255) NOT NULL,
     last_update_date TIMESTAMP NOT NULL,
-    FOREIGN KEY (request_id) REFERENCES virginia_dev_saayam_rdbms.request (request_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (request_id) REFERENCES virginia_dev_saayam_rdbms.requests (request_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (volunteer_id) REFERENCES virginia_dev_saayam_rdbms.users (user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
