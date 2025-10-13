4bcfd545aaa96aa82a72cfa44da474a2ba7b3bfc
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 21:10:37 -0400

    Update and rename ddl_user_availability.sql to ddl_user_availabilities.sql

--- ddl/Tables/ddl_user_availabilities.sql
+++ ddl/Tables/ddl_user_availabilities.sql
@@ -1,5 +1,5 @@
--- Table: user_availability
-CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_availability (
+-- Table: user_availabilities
+CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_availabilities (
     user_availability_id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     day_of_week VARCHAR(10) CHECK (day_of_week IN ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')),
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_availability (
     FOREIGN KEY (user_id) REFERENCES virginia_dev_saayam_rdbms.users (user_id) 
 );
 
--- Indexes for user_availability
-CREATE INDEX IF NOT EXISTS idx_user_availability_user_id ON virginia_dev_saayam_rdbms.user_availability (user_id);
+-- Indexes for user_availabilities
+CREATE INDEX IF NOT EXISTS idx_user_availability_user_id ON virginia_dev_saayam_rdbms.user_availabilities (user_id);
