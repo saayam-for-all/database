a215656c28c50734d5ffaf71e22ab3c7ece96e7a
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 20:55:04 -0400

    Update and rename ddl_country.sql to ddl_countries.sql

--- ddl/Tables/ddl_countries.sql
+++ ddl/Tables/ddl_countries.sql
@@ -1,5 +1,5 @@
--- Table: country
-CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.country (
+-- Table: countries
+CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.countries (
     country_id SERIAL PRIMARY KEY,
     country_name VARCHAR(255) NOT NULL,
     phone_country_code INT NOT NULL,
