9d742a30855e42ee9022088c37c055f55ff86766
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 21:05:00 -0400

    Update and rename ddl_request_status.sql to ddl_request_statuses.sql

--- ddl/Tables/ddl_request_statuses.sql
+++ ddl/Tables/ddl_request_statuses.sql
@@ -1,5 +1,5 @@
--- Table: request_status
-CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_status (
+-- Table: request_statuses
+CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_statuses (
     req_status_id SERIAL PRIMARY KEY,
     req_status VARCHAR(25) NOT NULL,
     req_status_desc VARCHAR(125),
