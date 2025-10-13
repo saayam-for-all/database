62ee506d2646db97e827dc1e79ecc7c3bf0d9500
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 21:02:52 -0400

    Update ddl_req_other_details.sql

--- ddl/Tables/ddl_req_other_details.sql
+++ ddl/Tables/ddl_req_other_details.sql
@@ -1,6 +1,6 @@
--- This table is only populated when req_for is 1/OTHER  in the 'request' table.
+-- This table is only populated when req_for is 1/OTHER  in the 'requests' table.
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_guest_details (
-    -- The primary key is the foreign key to the request table (one-to-one relationship)
+    -- The primary key is the foreign key to the requests table (one-to-one relationship)
     req_id VARCHAR(255) PRIMARY KEY,
     req_fname VARCHAR(100) NOT NULL,
     req_lname VARCHAR(100) NOT NULL,
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_guest_details (
     req_age INT,
     req_gender VARCHAR(50),
     req_pref_lang VARCHAR(50),
-    FOREIGN KEY (req_id) REFERENCES virginia_dev_saayam_rdbms.request (req_id) ON DELETE CASCADE
+    FOREIGN KEY (req_id) REFERENCES virginia_dev_saayam_rdbms.requests (req_id) ON DELETE CASCADE
 );
 
