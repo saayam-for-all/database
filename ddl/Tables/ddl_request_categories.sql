6e884a5e4d45229c40b19fcb965f651c04347a33
Gabbaraju09 <gowthamiabbaraju@gmail.com>
2025-10-12 21:03:49 -0400

    Update and rename ddl_request_category.sql to ddl_request_categories.sql

--- ddl/Tables/ddl_request_categories.sql
+++ ddl/Tables/ddl_request_categories.sql
@@ -1,5 +1,5 @@
--- Table: request_category REMOVE THIS AS WE HAVE SEPERATE HELP CATEGORIES SUB SCHEMA
-CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_category (
+-- Table: request_categories REMOVE THIS AS WE HAVE SEPERATE HELP CATEGORIES SUB SCHEMA
+CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_categories (
     request_category_id SERIAL PRIMARY KEY,
     request_category VARCHAR(255) NOT NULL,
     request_category_desc VARCHAR(255),
