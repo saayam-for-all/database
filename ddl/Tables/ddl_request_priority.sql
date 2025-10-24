0fd4017e817028c5b496faf6ce4516996a349fd8
PallaviChitrada <pallavichitrada10@gmail.com>
2025-10-23 21:16:49 -0500

    Fix unique constraint on req_priority_id

--- ddl/Tables/ddl_request_priority.sql
+++ ddl/Tables/ddl_request_priority.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.request_priority (
     req_priority VARCHAR(25) NOT NULL,
     req_priority_desc VARCHAR(125),
     last_updated_date TIMESTAMP,
-    UNIQUE (priority_id)
+    UNIQUE (req_priority_id)
 );
 
 
