e4bec3f42a85840be26f05e0b65e79e3bbba417f
PallaviChitrada <pallavichitrada10@gmail.com>
2025-11-13 02:07:42 -0500

    Modify state table schema in ddl_state.sql
    Changed state_id to VARCHAR(50) and updated state_name to VARCHAR(100). Added state_code column and removed unique constraint.

--- ddl/Tables/ddl_state.sql
+++ ddl/Tables/ddl_state.sql
@@ -1,9 +1,9 @@
 -- Table: state
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.state (
-    state_id SERIAL PRIMARY KEY,
+    state_id VARCHAR(50) PRIMARY KEY,
     country_id INT NOT NULL,
-    state_name VARCHAR(255) NOT NULL,
+    state_name VARCHAR(100) NOT NULL,
+    state_code VARCHAR(6),
     last_update_date TIMESTAMP,
-    UNIQUE (country_id, state_id),
     FOREIGN KEY (country_id) REFERENCES virginia_dev_saayam_rdbms.country (country_id)
 );
