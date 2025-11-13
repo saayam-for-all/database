e193e2ffea7d7be3fab7a6c53bff562dabd1cedb
PallaviChitrada <pallavichitrada10@gmail.com>
2025-11-13 02:07:04 -0500

    Modify country table schema for name and codes

--- ddl/Tables/ddl_country.sql
+++ ddl/Tables/ddl_country.sql
@@ -1,8 +1,9 @@
 -- Table: country
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.country (
     country_id SERIAL PRIMARY KEY,
-    country_name VARCHAR(255) NOT NULL,
-    phone_country_code INT NOT NULL,
+    country_name VARCHAR(100) NOT NULL,
+    phone_code VARCHAR(5) NOT NULL,
+    country_code VARCHAR(6) NOT NULL,
     last_update_date TIMESTAMP,
     UNIQUE (country_id)
 );
