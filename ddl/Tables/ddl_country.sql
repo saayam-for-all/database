db4d8d7aaf1e58b48a9513b2108a194023d9d8d8
vikashinisabarinathan <vs3224@g.rit.edu>
2025-11-17 16:05:15 -0800

    Added is_eu_member column to country table

--- ddl/Tables/ddl_country.sql
+++ ddl/Tables/ddl_country.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.country (
     country_name VARCHAR(255) NOT NULL,
     phone_country_code INT NOT NULL,
     last_update_date TIMESTAMP,
+    is_eu_member BOOLEAN DEFAULT FALSE,
     UNIQUE (country_id)
 );
