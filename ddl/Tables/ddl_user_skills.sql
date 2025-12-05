0f181734c4f729a40aff724fa061b3ef5a696dce
Joshitha Sai Uppalapati <uppalapatijoshithasai@gmail.com>
2025-12-04 22:04:18 -0500

    Updated volunteer_details and user_skills DDL; removed deprecated tables

--- ddl/Tables/ddl_user_skills.sql
+++ ddl/Tables/ddl_user_skills.sql
@@ -1,14 +1,4 @@
-CREATE TYPE skill_levels AS ENUM('BEGINNER', 'INTERMEDIATE', 'ADVANCED', 'EXPERT');
-
--- Table: user_skills
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_skills (
-	user_skills_id SERIAL PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
-    skill_id INT NOT NULL,
-    skill_level skill_levels,
-    last_used_date TIMESTAMP,
-    created_date TIMESTAMP,
-    last_update_date TIMESTAMP,
-    FOREIGN KEY (skill_id) REFERENCES virginia_dev_saayam_rdbms.skill_list (skill_list_id),
-    FOREIGN KEY (user_id) REFERENCES virginia_dev_saayam_rdbms.users (user_id)
+    user_id uuid PRIMARY KEY,
+    skills jsonb
 );
