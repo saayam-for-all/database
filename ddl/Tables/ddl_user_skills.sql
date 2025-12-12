637f17a179f34676753c3e72a4d0ce9b8167922d
Joshitha Sai Uppalapati <uppalapatijoshithasai@gmail.com>
2025-12-11 19:22:24 -0500

    Normalize user_skills with FK to help_categories

--- ddl/Tables/ddl_user_skills.sql
+++ ddl/Tables/ddl_user_skills.sql
@@ -1,4 +1,20 @@
+DROP TABLE IF EXISTS virginia_dev_saayam_rdbms.user_skills CASCADE;
+
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_skills (
-    user_id uuid PRIMARY KEY,
-    skills jsonb
+    user_id uuid NOT NULL,
+    cat_id VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+
+    CONSTRAINT user_skills_pk
+        PRIMARY KEY (user_id, cat_id),
+
+    CONSTRAINT user_skills_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES virginia_dev_saayam_rdbms.users(user_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT user_skills_category_fk
+        FOREIGN KEY (cat_id)
+        REFERENCES virginia_dev_saayam_rdbms.help_categories(cat_id)
 );
