4677093295030b4cf44c8011133de34d97467cd8
Joshitha Sai Uppalapati <uppalapatijoshithasai@gmail.com>
2025-12-14 19:34:05 -0500

    Normalize user_skills and update volunteer_details schema

--- ddl/Tables/ddl_user_skills.sql
+++ ddl/Tables/ddl_user_skills.sql
@@ -1,20 +1,18 @@
 DROP TABLE IF EXISTS virginia_dev_saayam_rdbms.user_skills CASCADE;
 
-CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.user_skills (
+CREATE TABLE virginia_dev_saayam_rdbms.user_skills (
     user_id uuid NOT NULL,
-    cat_id VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
-    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+    cat_id varchar(50) NOT NULL,
+    created_at timestamp without time zone DEFAULT now(),
 
-    CONSTRAINT user_skills_pk
-        PRIMARY KEY (user_id, cat_id),
+    CONSTRAINT user_skills_pk PRIMARY KEY (user_id, cat_id),
 
     CONSTRAINT user_skills_user_fk
         FOREIGN KEY (user_id)
         REFERENCES virginia_dev_saayam_rdbms.users(user_id)
         ON DELETE CASCADE,
 
-    CONSTRAINT user_skills_category_fk
+    CONSTRAINT user_skills_cat_fk
         FOREIGN KEY (cat_id)
         REFERENCES virginia_dev_saayam_rdbms.help_categories(cat_id)
 );
