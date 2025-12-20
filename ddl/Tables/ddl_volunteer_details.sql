f1111963ee436594110ffb151e46c093d28ff188
Joshitha Uppalpati <uppalapatijoshithasai@gmail.com>
2025-12-20 00:33:20 -0500

    volunteer_details table schema

--- ddl/Tables/ddl_volunteer_details.sql
+++ ddl/Tables/ddl_volunteer_details.sql
@@ -3,27 +3,17 @@ DROP TABLE IF EXISTS virginia_dev_saayam_rdbms.volunteer_details CASCADE;
 CREATE TABLE virginia_dev_saayam_rdbms.volunteer_details (
     user_id VARCHAR(255) PRIMARY KEY,
 
-    terms_and_conditions boolean,
-    terms_and_conditions_update_date timestamp without time zone,
+    terms_and_conditions BOOLEAN,
+    terms_and_conditions_update_date TIMESTAMP WITHOUT TIME ZONE,
 
-    govt_id_path text,
-    govt_id_path2 text,
-    govt_id_update_date timestamp without time zone,
-    
+    govt_id_path1 TEXT,
+    govt_id_path2 TEXT,
+    path1_updated_at TIMESTAMP WITHOUT TIME ZONE,
+    path2_updated_at TIMESTAMP WITHOUT TIME ZONE,
 
-    notification boolean,
-    iscomplete boolean,
-    completed_date timestamp without time zone,
+    availability_days JSON,
+    availability_times JSON,
 
-    availability_days text[],
-    availability_time text,
-
-    location text,
-
-    created_at timestamp without time zone DEFAULT now()
-
---    CONSTRAINT volunteer_details_user_fk
---        FOREIGN KEY (user_id)
---        REFERENCES virginia_dev_saayam_rdbms.users(user_id)
---        ON DELETE CASCADE
+    created_at TIMESTAMP WITHOUT TIME ZONE,
+    updated_at TIMESTAMP WITHOUT TIME ZONE
 );
