98085d4c31f6719a3b75712dacf390fd7fb05768
PallaviChitrada <pallavichitrada10@gmail.com>
2025-12-24 13:31:46 -0500

    Add news snippet for Volunteer Drive Success

---
+++ dml/dml_news_snippets.sql
@@ -0,0 +1,8 @@
+INSERT INTO virginia_dev_saayam_rdbms.news_snippets (headline,snippet_text,image_path,profile_links,event_date
+) VALUES (
+'Volunteer Drive Success',
+'Our latest volunteer drive was a huge success! Over 50 new volunteers joined us to support our mission. Special thanks to CHP for leading the effort',
+'s3:---01.jpg',
+'[{"name": "CHP", "url": "https://linkedin.com/in/CHP"}]',
+'2025-12-15' -- The date the event actually happened
+);
