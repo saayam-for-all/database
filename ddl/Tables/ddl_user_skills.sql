ef4586534d7a9c405aa644c22db7de68b7c2ab9f
PallaviChitrada <pallavichitrada10@gmail.com>
2025-12-24 17:36:42 -0500

    Modify user_skills table structure and add triggers

--- ddl/Tables/ddl_user_skills.sql
+++ ddl/Tables/ddl_user_skills.sql
@@ -1,11 +1,18 @@
 DROP TABLE IF EXISTS virginia_dev_saayam_rdbms.user_skills CASCADE;
 
 CREATE TABLE virginia_dev_saayam_rdbms.user_skills (
-    user_id VARCHAR(255) NOT NULL,
-    cat_id INT NOT NULL,
-
-    created_at TIMESTAMP WITHOUT TIME ZONE,
-    updated_at TIMESTAMP WITHOUT TIME ZONE,
-
-    CONSTRAINT user_skills_pkey PRIMARY KEY (user_id, cat_id)
+    user_id VARCHAR(255),
+    cat_id VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
+    last_updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
+    PRIMARY KEY (user_id, cat_id),
+    FOREIGN KEY (user_id) REFERENCES virginia_dev_saayam_rdbms.users(user_id),
+    FOREIGN KEY (cat_id) REFERENCES virginia_dev_saayam_rdbms.help_categories(cat_id)
 );
+
+CREATE TRIGGER trg_user_skills_updated_at
+BEFORE UPDATE ON virginia_dev_saayam_rdbms.user_skills
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
+
+CREATE INDEX idx_user_skills_cat_id  ON virginia_dev_saayam_rdbms.user_skills (cat_id);
