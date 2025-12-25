92a567205b5a16fed6047d63540826aff817887b
PallaviChitrada <pallavichitrada10@gmail.com>
2025-12-24 19:10:23 -0500

    Refactor updated_at_handler for volunteer applications

--- ddl/Tables/ddl_volunteer_applications.sql
+++ ddl/Tables/ddl_volunteer_applications.sql
@@ -21,39 +21,39 @@ CREATE TABLE virginia_dev_saayam_rdbms.volunteer_applications (
 CREATE OR REPLACE FUNCTION virginia_dev_saayam_rdbms.updated_at_handler()
 RETURNS TRIGGER AS $$
 BEGIN
+    -- check if the last_updated_at was explicitly changed in the UPDATE statement.
+    -- if it WASN'T changed by the API, we force it to the current UTC.
+    IF (NEW.last_updated_at IS NOT DISTINCT FROM OLD.last_updated_at) THEN
+        NEW.last_updated_at = (now() AT TIME ZONE 'UTC');
+    END IF;
+
     -- handle application table 
     IF (TG_TABLE_NAME = 'volunteer_applications') THEN
-    
-        NEW.last_updated_at = COALESCE(NEW.last_updated_at, (now() AT TIME ZONE 'UTC')); -- for any attribute
-        
         -- Update path timestamp if path changed
         IF (NEW.govt_id_path IS DISTINCT FROM OLD.govt_id_path) THEN
-            NEW.path_updated_at = COALESCE(NEW.path_updated_at, (now() AT TIME ZONE 'UTC'));
+            NEW.path_updated_at = (now() AT TIME ZONE 'UTC');
         END IF;
 
         -- Update terms timestamp if accepted
         IF (NEW.terms_and_conditions IS TRUE AND (OLD.terms_and_conditions IS FALSE OR OLD.terms_and_conditions IS NULL)) THEN
-            NEW.terms_accepted_at = COALESCE(NEW.terms_accepted_at, (now() AT TIME ZONE 'UTC'));
+            NEW.terms_accepted_at = (now() AT TIME ZONE 'UTC');
         END IF;
 
     -- handle details table 
     ELSIF (TG_TABLE_NAME = 'volunteer_details') THEN
-        
-        NEW.last_updated_at = COALESCE(NEW.last_updated_at, (now() AT TIME ZONE 'UTC')); -- for any attribute
-        
         -- update path1 timestamp if changed
         IF (NEW.govt_id_path1 IS DISTINCT FROM OLD.govt_id_path1) THEN
-            NEW.path1_updated_at = COALESCE(NEW.path1_updated_at, (now() AT TIME ZONE 'UTC'));
+            NEW.path1_updated_at = (now() AT TIME ZONE 'UTC');
         END IF;
         
         -- update path2 timestamp if changed
         IF (NEW.govt_id_path2 IS DISTINCT FROM OLD.govt_id_path2) THEN
-            NEW.path2_updated_at = COALESCE(NEW.path2_updated_at, (now() AT TIME ZONE 'UTC'));
+            NEW.path2_updated_at = (now() AT TIME ZONE 'UTC');
         END IF;
 
         -- update terms timestamp if accepted
         IF (NEW.terms_and_conditions IS TRUE AND (OLD.terms_and_conditions IS FALSE OR OLD.terms_and_conditions IS NULL)) THEN
-            NEW.terms_accepted_at = COALESCE(NEW.terms_accepted_at, (now() AT TIME ZONE 'UTC'));
+            NEW.terms_accepted_at = (now() AT TIME ZONE 'UTC');
         END IF;
     END IF;
 
@@ -69,11 +69,11 @@ BEGIN
         -- Migrate to volunteer_details
         INSERT INTO virginia_dev_saayam_rdbms.volunteer_details (
             user_id, terms_and_conditions, terms_accepted_at, 
-            govt_id_path1, availability_days, availability_times, 
+            govt_id_path1, path1_updated_at, path2_updated_at, availability_days, availability_times, 
             created_at, last_updated_at
         ) VALUES (
             NEW.user_id, NEW.terms_and_conditions, NEW.terms_accepted_at,
-            NEW.govt_id_path, (NEW.availability -> 'days')::JSONB, (NEW.availability -> 'time')::JSONB,
+            NEW.govt_id_path, NEW.path_updated_at, NULL, (NEW.availability -> 'days')::JSONB, (NEW.availability -> 'time')::JSONB,
             NEW.last_updated_at, NEW.last_updated_at
         );
 
@@ -89,6 +89,8 @@ BEGIN
                 NEW.last_updated_at
             FROM json_array_elements_text(NEW.skill_codes) AS skill_id;
         END IF;
+
+		DELETE FROM virginia_dev_saayam_rdbms.volunteer_applications WHERE user_id = NEW.user_id;
     END IF;
     
     RETURN NEW;
@@ -102,3 +104,5 @@ CREATE TRIGGER trg_volunteer_app_updated_at
 CREATE TRIGGER trg_handle_volunteer_application
     AFTER UPDATE ON virginia_dev_saayam_rdbms.volunteer_applications
     FOR EACH ROW EXECUTE FUNCTION virginia_dev_saayam_rdbms.handle_volunteer_application();
+
+ALTER TABLE virginia_dev_saayam_rdbms.volunteer_applications ENABLE TRIGGER trg_volunteer_app_updated_at;
