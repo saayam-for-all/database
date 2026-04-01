9869b4fa5fafdcb40ddce6ef8dc26a2eeadce675
Vikashini Sabarinathan <vs3224@g.rit.edu>
2026-04-01 00:54:24 -0700

    Update ddl_supporting_languages.sql
    Updated iso_code column name

--- ddl/Tables/ddl_supporting_languages.sql
+++ ddl/Tables/ddl_supporting_languages.sql
@@ -2,16 +2,16 @@
 CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.supporting_languages (
     language_id       BIGSERIAL PRIMARY KEY,
     language_name     VARCHAR(64)  NOT NULL,
-    iso_639_1_code CHAR(2) NOT NULL,                 -- e.g., en, zh, hi
+    iso_code CHAR(2) NOT NULL,                 -- e.g., en, zh, hi
     locale_code       VARCHAR(10)  NOT NULL,                 -- e.g., en_US, zh_CN
     writing_direction VARCHAR(3)   NOT NULL DEFAULT 'LTR',   -- 'LTR' or 'RTL'
     total_speakers_m  NUMERIC(10,1),                         -- millions (optional)
     is_active         BOOLEAN      NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     last_updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT uq_lang_unique UNIQUE (iso_639_1_code, locale_code),
+    CONSTRAINT uq_lang_unique UNIQUE (iso_code, locale_code),
     CONSTRAINT ck_direction CHECK (writing_direction IN ('LTR','RTL')),
-    CONSTRAINT ck_iso6391 CHECK (iso_639_1_code ~ '^[A-Za-z]{2}$'),
+    CONSTRAINT ck_iso6391 CHECK (iso_code ~ '^[A-Za-z]{2}$'),
     CONSTRAINT ck_locale CHECK (locale_code ~ '^[a-z]{2}_[A-Z]{2}$')
 );
 
