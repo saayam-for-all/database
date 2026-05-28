47d8e86d5fa4fe2f2e3090ba72a7d7c0fa444171
Mohith Seelam <88337483+MohithS04@users.noreply.github.com>
2026-05-28 19:24:30 -0400

    Refactor organization table structure and constraints
    Renamed 'is_collaborator' to 'is_collaborative' and added 'is_contributing' column. Updated constraints and ensured proper default values for timestamps.

--- ddl/Tables/ddl_organizations.sql
+++ ddl/Tables/ddl_organizations.sql
@@ -18,9 +18,12 @@ CREATE TABLE IF NOT EXISTS virginia_dev_saayam_rdbms.organizations (
   org_type org_type_enum,
   org_size org_size_enum,
   org_rating INTEGER CHECK (org_rating >= 1 AND org_rating <= 5),
-  is_collaborator BOOLEAN,
+  is_collaborative BOOLEAN,
+  is_contributing BOOLEAN,
   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
-  last_updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),  
+  last_updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'UTC'),
+  CONSTRAINT chk_contributing_only_for_profit
+      CHECK (org_type = 'for_profit' OR is_contributing IS NULL),
   FOREIGN KEY (state_id) REFERENCES states(state_id) ON DELETE SET NULL
 );
 
